6d433428786865c8ba520f8538affa91e1aa5c92
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-22 01:29:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,4 +102,21 @@ def changing_alan
    	programmer_hash
 end
 def adding_to_dennis
-       pro
+  programmer_hash =
+{
+   :grace_hopper => {
+     :known_for => "COBOL",
+     :languages => ["COBOL", "FORTRAN"]
+   },
+   :alan_kay => {
+     :known_for => "Object Orientation",
+     :languages => ["Smalltalk", "LISP"]
+   },
+   :dennis_ritchie => {
+     :known_for => "Unix",
+     :languages => ["C"]
+   }
+}
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
+end
